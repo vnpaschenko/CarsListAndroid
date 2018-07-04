b3464a3637ab40608560c1a67c285df9394a8e84
vnpaschenko <vnpaschenko@gmail.com>
2018-07-04 23:59:23 +0300

    Update build environment

--- app/src/androidTest/java/com/example/ipaschenko/carslist/data/CarsListCsvParserTest.kt
+++ app/src/androidTest/java/com/example/ipaschenko/carslist/data/CarsListCsvParserTest.kt
@@ -1,13 +1,16 @@
 package com.example.ipaschenko.carslist.data
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Test parsing our assets
  */
+@RunWith(AndroidJUnit4::class)
 class CarsListCsvParserTest {
     @Test
     fun testParsing() {
